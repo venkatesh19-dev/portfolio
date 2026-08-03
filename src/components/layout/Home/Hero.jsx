ca010e5411d1df9896d28e9f76cb012d9cc1446b
Venkatesh <savaujaklaptop@gmail.com>
2026-08-04 02:13:06 +0530

    Updated hero section

--- src/components/layout/Home/Hero.jsx
+++ src/components/layout/Home/Hero.jsx
@@ -16,8 +16,8 @@ const Hero = () => {
         className="absolute inset-0"
         style={{
           backgroundImage: `
-            linear-gradient(rgba(255,255,255,.05) 1px, transparent 1px),
-            linear-gradient(90deg, rgba(255,255,255,.05) 1px, transparent 1px)
+            linear-gradient(rgba(255,255,255, 0.8) 1px, transparent 1px),
+            linear-gradient(90deg, rgba(255,255,255, 0.8) 1px, transparent 1px)
           `,
           backgroundSize: "70px 70px",
         }}
